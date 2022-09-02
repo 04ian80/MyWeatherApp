2778b390709ada806186a53b5411bf701a66abf6
Ain Chung <artnouveau0804@gmail.com>
2022-09-02 14:47:51 +0900

    [4] Styles
    margin은 마이너스로 줄 수도 있음.

--- App.js
+++ App.js
@@ -4,16 +4,52 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <>
-      <View style={{ flex: 4 }}>
-        <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-        <View style={{ flex: 6, backgroundColor: 'teal' }}></View>
-        <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
-      </View>
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-        <View style={{ flex: 6, backgroundColor: 'teal' }}></View>
-        <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Jeju</Text>
+        </View>
+        <View style={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>23</Text>
+            <Text style={styles.desc}>Sunny</Text>
+          </View>
+        </View>
+        <StatusBar style='light' />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1D1D1D',
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 38,
+    fontWeight: '500',
+    color: 'white',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 98,
+    color: 'white',
+  },
+  desc: {
+    marginTop: -10,
+    fontSize: 32,
+    color: 'white',
+  },
+});
