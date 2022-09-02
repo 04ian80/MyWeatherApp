9656e7a6a684c3d950c7491ad117163dcdd0432b
Ain Chung <artnouveau0804@gmail.com>
2022-09-02 14:24:09 +0900

    [3] Layout System
    1. 모바일에서는 display: grid, inline-block, block 다 없고 flexbox만 있음. 그리고 flexDirection 기본값이 column임.
    그리고 요소가 화면 밖을 나갔다고 해서 스크롤이 되는게 아님.
    
    2. 모바일 화면 크기는 다 다르기 때문에 width와 height 속성은 정하지 않는 경우가 대부분임. 그래서 비율로 줌!!!!
    flex: 1을 부모 요소에 넣어주고, 자식요소에도 부모와 똑같이 넣어주면 비율에 맞춰서 나옴.
    View 컨테이너가 하나인 상황에서는 1이상을 줘도 아무 반응이 없지만 두개 이상인 상황에서는 비율대로 움직임.

--- App.js
+++ App.js
@@ -3,22 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style='light' />
-    </View>
+    <>
+      <View style={{ flex: 4 }}>
+        <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
+        <View style={{ flex: 6, backgroundColor: 'teal' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+      </View>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
+        <View style={{ flex: 6, backgroundColor: 'teal' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    // backgroundColor: '#000',
-  },
-  text: {
-    fontSize: 32,
-  },
-});
